77ce4462e580a067ef7d8bd79513d1f9d7f91971
sardor0092 <sardorkhaydarov099@gmail.com>
2023-09-12 22:57:01 +0500

    7-guruh darslaridan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  FormArray, FormArrayName, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+   
+   
+   
+    
+    
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+  
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
+
+
+
+
 export class AppModule { }
